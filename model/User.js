0392b155148220bc2b000eca58ebf74177cafbf3
Nahid005 <nahidhassan9258@gmail.com>
2022-07-14 15:01:42 +0600

    login and register api design

--- model/User.js
+++ model/User.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    roles: [String],
-    accountsStatus: String
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
+    password: {
+        type: String,
+        minlength: 8
+    },
+    roles: {
+        type: [String],
+        default: ['student']
+    },
+    accountsStatus: {
+        type: String,
+        enum: ["PENDING", "ACTIVE", "REJECT"],
+        default:"PENDING"
+    }
 })
 
 const User = mongoose.model('User', userSchema)
