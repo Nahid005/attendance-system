054acab6677f0618789f92fd33a7568784b16660
Nahid005 <nahidhassan9258@gmail.com>
2022-07-21 00:27:17 +0600

    user api design

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,6 @@
 const User = require('../model/User')
 const userService = require('../services/user')
+const authService = require('../services/auth')
 
 const getUserController = async (_req, res, next) => {
     try{
@@ -13,21 +14,81 @@ const getUserController = async (_req, res, next) => {
     }
 }
 
-const postUserController = (req, res, next) => {
+const postUserController = async (req, res, next) => {
+
+    const {name, email, password, roles, accountsStatus} = req.body
+
+    if(!name ?? !email ?? password ?? roles ?? accountsStatus){
+        res.status(400).json({message: "invalide data "})
+    }
+
     try{
 
-
+        const user = await authService.registrationService({name, email, password, roles, accountsStatus})
+        return res.status(201).json({message: 'create new user', user})
 
     }catch(e) {
         next(e)
     }
 }
 
-const putUserController = () => {}
+const putUserController = async (req, res, next) => {
+    const userId = req.params.userId
+    const { name, email, roles, accountsStatus } = req.body
 
-const patchUserController = () => {}
+    try{
 
-const deleteUserController = () => {}
+        const user = await userService.patUpdateUser(userId, {name, email, roles, accountsStatus})
+
+        if(!user){
+            res.status(404).json({message: 'user not found'})
+        }
+
+        return res.status(200).json(user)
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const patchUserController = async (req, res, next) => {
+    const userId = req.params.userId
+    const { name, roles, accountsStatus } = req.body
+
+    try{
+        const user = await userService.userFindByProperty('_id', userId)
+
+        user.name = name ?? user.name,
+        user.roles = roles ?? user.roles,
+        user.accountsStatus = accountsStatus ?? user
+        
+        user.save()
+
+        return res.status(200).json(user)
+
+    } catch(e){
+        next(e)
+    }
+}
+
+const deleteUserController = async (req, res, next) => {
+
+    const userId = req.params.userId
+
+    try{
+        const user = await userService.userFindByProperty('_id', userId)
+        console.log(user)
+        if(!user) {
+            res.status(200).json({message: 'user not found'})
+        }
+
+        user.remove()
+        res.status(200).json({message: 'delete user'})
+
+    }catch(e) {
+        next(e)
+    }
+}
 
 const getUserByIdController = async (req, res, next) => {
 
